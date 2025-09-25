67ba5217b47d90b9516589de61d293697467a57b
Matteo Copelli <emmecoop.consulting@gmail.com>
2025-09-25 14:25:30 +0200

    caricamento codice modulo

---
+++ partner_iscritti/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Partner Iscritti (Asilo Nido)",
+    "version": "18.0.1.0.0",
+    "category": "Contacts",
+    "summary": "Tab 'Asilo Nido' su Contatti con elenco iscritti collegati al genitore",
+    "author": "Emme Cop Consulting",
+    "license": "LGPL-3",
+    "depends": ["base", "contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ partner_iscritti/models/pb_nido_iscritto.py
@@ -0,0 +1,34 @@
+from odoo import models, fields
+
+class PbNidoIscritto(models.Model):
+    _name = "pb.nido.iscritto"
+    _description = "Iscritto Asilo Nido"
+    _order = "cognome, nome, id"
+
+    parent_id = fields.Many2one(
+        'res.partner',
+        string="Genitore",
+        required=True,
+        ondelete='cascade',
+        index=True,
+        help="Partner genitore a cui è associato l'iscritto."
+    )
+    nome = fields.Char("Nome", required=True)
+    cognome = fields.Char("Cognome", required=True)
+    data_nascita = fields.Date("Data di nascita", required=True)
+    comune_nascita = fields.Char("Comune di nascita")
+    codice_fiscale = fields.Char("Codice Fiscale", size=16, help="16 caratteri")
+    protocollo_iscrizione = fields.Char("Protocollo iscrizione")
+
+    _sql_constraints = [
+        ('cf_len_16',
+         "CHECK (codice_fiscale IS NULL OR length(codice_fiscale)=16)",
+         "Il Codice Fiscale deve avere 16 caratteri."),
+    ]
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    nido_iscritto_ids = fields.One2many(
+        'pb.nido.iscritto', 'parent_id',
+        string="Iscritti"
+    )
